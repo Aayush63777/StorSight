ece577cfdf00f04217e67870c2dc97d4b02773c3
Aayush Kumar <ayushkumar7568@gmail.com>
2026-09-13 23:38:29 +0530

    add Vercel speed insights

--- storsight-frontend/src/main.ts
+++ storsight-frontend/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+injectSpeedInsights({ framework: 'angular' });
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
